917b57c76f422fd0dab4256438e294b96ebeb18d
Blairi <axel.avi.fer@gmail.com>
2023-01-21 14:59:11 -0600

    using lang context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import '../styles/bg.css'
 
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
+import { LangContext } from '../context/LangContext';
 
 const Home = () => {
+
+  const { lang_dict } = useContext(LangContext);
+
   return (
     <div name='home' className='w-full flex justify-center h-screen items-center bg'>
 
@@ -14,12 +18,11 @@ const Home = () => {
         className='w-[93%] md:w-auto max-w-[1000px] md:mx-auto py-2 px-2 md:p-8 flex flex-col justify-center bg-black rounded-md'
       >
 
-        <p className='text-purple-500 text-base font-bold'>Hola, mi nombre es</p>
-        <h1 className='text-3xl md:text-7xl font-bold text-[#ccd6f6]'>&#60; Axel Montiel &#47;&#62;</h1>
-        <h2 className='text-2xl md:text-6xl font-bold text-[#8892b0]'>Soy programador.</h2>
+        <p className='text-purple-500 text-base font-bold'>{ lang_dict.presentation.intro }</p>
+        <h1 className='text-3xl md:text-7xl font-bold text-[#ccd6f6]'>&#60; { lang_dict.presentation.name } &#47;&#62;</h1>
+        <h2 className='text-2xl md:text-6xl font-bold text-[#8892b0]'>{ lang_dict.presentation.occupation }</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px] text-lg'>
-          Desarollador de aplicaciones web, apasionado de investigar y construir aplicaciones.
-          Actualmente estudiante de ingeniería en computación, en UNAM.
+          { lang_dict.presentation.info }
         </p>
 
         <div >
@@ -27,7 +30,7 @@ const Home = () => {
             <button
               className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
             >
-              Ver proyectos
+              {lang_dict.buttons.projects}
               <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3 ' />
               </span>
