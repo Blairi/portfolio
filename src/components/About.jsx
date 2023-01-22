34e33b373ee7bd3fa0de6d9dc4f6815bb142bd57
Blairi <axel.avi.fer@gmail.com>
2023-01-22 08:30:12 -0600

    using about texts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import { useContext } from 'react'
+import { LangContext } from '../context/LangContext'
 
-const about = () => {
+const About = () => {
+
+  const { lang_dict } = useContext( LangContext );
+
   return (
     <div name='about' className='w-full flex items-center bg-[#102037] text-gray-300 py-36'>
       <div className='w-full grid place-items-center'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Acerca</p>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>{ lang_dict.about.about }</p>
           </div>
           <div>
           </div>
@@ -14,14 +18,16 @@ const about = () => {
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div>
             <p className='sm:text-right text-4xl font-bold'>
-              Hola, gracias por visitar mi portafolió. Aquí un poco acerca de mí.
+              { lang_dict.about.intro }
             </p>
           </div>
           <div>
             <p className='text-lg'>
-              Estudié en una preparatoría tecnica en la que lleve la carrera de: "Tecnico en programación". Durante la preparatoría aprendí a programar desde diagramas de flujo con DFD, páginas web, hasta crear aplicaciones moviles con Java. Al concluir mis estudios en la preparatoría decidi ampliar mis conocimientos y estudié desarollo web con cursos de internet; con una buena base de programación, fue facíl para mí adaptarme. Ahora curso una ingeniería donde llevo materias de programación como estructuras de datos y demas.
+              { lang_dict.about.bio }
             </p>
-            <a className='text-pink-600 underline text-lg font-bold' href="https://www.linkedin.com/in/axel-montiel-000848202/">Mi Linkedin</a>
+            <a className='text-pink-600 underline text-lg font-bold' href="https://www.linkedin.com/in/axel-montiel-000848202/">
+              { lang_dict.about.linkedin }
+            </a>
           </div>
         </div>
       </div>
@@ -29,4 +35,4 @@ const about = () => {
   )
 }
 
-export default about
+export default About
