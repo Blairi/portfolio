aedd66ff589c2fca425d8b81b1b2f799398a02e9
Blairi <axel.avi.fer@gmail.com>
2023-01-22 08:37:12 -0600

    using skills texts

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import '../styles/bg.css'
 
@@ -16,8 +16,12 @@ import reactIcon from '../assets/react.png';
 import reduxIcon from '../assets/redux.png';
 
 import TechnologyIcon from './ui/TechnologyIcon';
+import { LangContext } from '../context/LangContext';
 
 const Skills = () => {
+
+  const { lang_dict } = useContext( LangContext );
+
   return (
     <div name='skills' className='w-full bg text-gray-300 py-36 overflow-hidden'>
       
@@ -25,8 +29,8 @@ const Skills = () => {
       <div className='mx-auto flex flex-col justify-center w-[95%] max-w-[1000px] rounded-md bg-black p-4 md:p-8'>
 
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-purple-500'>Habilidades</p>
-          <p className='py-4'>&#47;&#47; Aquí algunas de las tecnologías con las que he trabajado.</p>
+          <p className='text-4xl font-bold inline border-b-4 border-purple-500'>{ lang_dict.skills.skills }</p>
+          <p className='py-4'>&#47;&#47; { lang_dict.skills.subtitle }</p>
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center'>
