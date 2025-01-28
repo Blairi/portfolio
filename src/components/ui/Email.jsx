3c049a199d0687fad03cefa64c2d14a0ef7e7137
blairi <axel.avi.fer@gmail.com>
2025-01-27 21:48:21 -0600

    cambiando email

--- src/components/ui/Email.jsx
+++ src/components/ui/Email.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Email = () => {
 
   const data = {
-    user: 'axel.montiel',
-    domain: 'ieee.org',
+    user: 'axel.avi.fer',
+    domain: 'gmail.com',
   }
 
   return (
